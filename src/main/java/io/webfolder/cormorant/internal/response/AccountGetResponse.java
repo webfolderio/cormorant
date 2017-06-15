5a1d4c2e81d25c291c9d94a4af244a858a7efa80
webfolderio <support@webfolder.io>
2017-06-16 01:58:10 +0300

    Update e-mail address

--- src/main/java/io/webfolder/cormorant/internal/response/AccountGetResponse.java
+++ src/main/java/io/webfolder/cormorant/internal/response/AccountGetResponse.java
@@ -1,6 +1,6 @@
 /**
  * cormorant - Object Storage Server
- * Copyright © 2017 WebFolder OÜ (cormorant@webfolder.io)
+ * Copyright © 2017 WebFolder OÜ (support@webfolder.io)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
--- src/test/java/io/webfolder/cormorant/test/TestServer.java
+++ src/test/java/io/webfolder/cormorant/test/TestServer.java
@@ -1,7 +1,26 @@
+/**
+ * cormorant - Object Storage Server
+ * Copyright © 2017 WebFolder OÜ (support@webfolder.io)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.webfolder.cormorant.test;
 
 import static java.util.Locale.ENGLISH;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Locale;
@@ -15,13 +34,20 @@ import io.webfolder.cormorant.api.service.AuthenticationService;
 public class TestServer {
 
     public static void main(String[] args) {
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "debug");
+
         Locale.setDefault(ENGLISH);
 
         CormorantServer server = new CormorantServer();
         Path objectStore = Paths.get("mydir");
         Path metadataStore = Paths.get("mymetadata");
 
-        server.setHost("localhost");
+        try {
+            server.setHost(InetAddress.getLocalHost().getHostAddress());
+            System.out.println(server.getHost());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
 
         AccountService accountService = new TestAccountService(objectStore);
         AuthenticationService authenticationService = new TestAuthenticationService();
